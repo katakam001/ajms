60001a5954683654bfc0b91eb342120055e80b00
bkatakam <katakam001@gmail.com>
2024-05-29 13:23:09 +0530

    Completed the trail balance changes and added database dump.

--- admin/reports/working_trial_balance.php
+++ admin/reports/working_trial_balance.php
@@ -1,80 +1,85 @@
 <?php
-function format_num($number){
-	$decimals = 0;
-	$num_ex = explode('.',$number);
-	$decimals = isset($num_ex[1]) ? strlen($num_ex[1]) : 0 ;
-	return number_format($number,2);
+function format_num($number)
+{
+    $decimals = 0;
+    $num_ex = explode('.', $number);
+    $decimals = isset($num_ex[1]) ? strlen($num_ex[1]) : 0;
+    return number_format($number, 2);
 }
-function getfinancialYear($YearStart,$YearEnd){
-	return date("Y" , strtotime($YearStart))  . '-' . date("Y",strtotime($YearEnd));
+function getfinancialYear($YearStart, $YearEnd)
+{
+    return date("Y", strtotime($YearStart))  . '-' . date("Y", strtotime($YearEnd));
 }
-function getfinancialStartYear($Year){
-	return "{$Year}-04-01";
+function getfinancialStartYear($Year)
+{
+    return "{$Year}-04-01";
 }
-function getfinancialEndYear($Year){
-	return "{$Year}-03-31";
+function getfinancialEndYear($Year)
+{
+    return "{$Year}-03-31";
 }
 $financialYearStart =  getfinancialStartYear(date("Y")); // Get the current year
-$financialYearEnd = getfinancialEndYear(date("Y")+1);
+$financialYearEnd = getfinancialEndYear(date("Y") + 1);
 $from = isset($_GET['from']) ? $_GET['from'] :  $financialYearStart;
 $to = isset($_GET['to']) ? $_GET['to'] : $financialYearEnd;
-$financialYear = getfinancialYear($financialYearStart,$financialYearEnd);
-$id= $_settings->userdata('id');
+$financialYear = getfinancialYear($financialYearStart, $financialYearEnd);
+$id = $_settings->userdata('id');
 ?>
 <style>
-	th.p-0, td.p-0{
-		padding: 0 !important;
-	}
+    th.p-0,
+    td.p-0 {
+        padding: 0 !important;
+    }
 </style>
 <div class="card card-outline card-primary">
-	<div class="card-header">
-        <h3 class="card-title">Working Trial Balance for financial Year: <?=  $financialYear  ?></h3>
-		<div class="card-tools">
-		</div>
-	</div>
-	<div class="card-body">
+    <div class="card-header">
+        <h3 class="card-title">Working Trial Balance for financial Year: <?= $financialYear  ?></h3>
+        <div class="card-tools">
+        </div>
+    </div>
+    <div class="card-body">
         <div class="callout border-primary shadow rounded-0">
             <h4 class="text-muted">Filter Date</h4>
             <form action="" id="filter">
-            <div class="row align-items-end">
-                <div class="col-md-4 form-group">
-                    <label for="from" class="control-label">Date From</label>
-                    <input type="date" id="from" name="from" value="<?= $from ?>" class="form-control form-control-sm rounded-0">
+                <div class="row align-items-end">
+                    <div class="col-md-4 form-group">
+                        <label for="from" class="control-label">Date From</label>
+                        <input type="date" id="from" name="from" value="<?= $from ?>" class="form-control form-control-sm rounded-0">
+                    </div>
+                    <div class="col-md-4 form-group">
+                        <label for="to" class="control-label">Date To</label>
+                        <input type="date" id="to" name="to" value="<?= $to ?>" class="form-control form-control-sm rounded-0">
+                    </div>
+                    <div class="col-md-4 form-group">
+                        <button class="btn btn-default bg-gradient-navy btn-flat btn-sm"><i class="fa fa-filter"></i> Filter</button>
+                        <button class="btn btn-default border btn-flat btn-sm" id="print" type="button"><i class="fa fa-print"></i> Print</button>
+                    </div>
                 </div>
-                <div class="col-md-4 form-group">
-                    <label for="to" class="control-label">Date To</label>
-                    <input type="date" id="to" name="to" value="<?= $to ?>" class="form-control form-control-sm rounded-0">
-                </div>
-                <div class="col-md-4 form-group">
-                    <button class="btn btn-default bg-gradient-navy btn-flat btn-sm"><i class="fa fa-filter"></i> Filter</button>
-			        <button class="btn btn-default border btn-flat btn-sm" id="print" type="button"><i class="fa fa-print"></i> Print</button>
-                </div>
-            </div>
             </form>
         </div>
         <div class="container-fluid" id="outprint">
             <h3 class="text-center"><b><?= $_settings->info('name') ?></b></h3>
             <h4 class="text-center"><b>Working Trial Balance</b></h4>
-            <?php if($from == $to): ?>
-            <p class="m-0 text-center"><?= date("M d, Y" , strtotime($from)) ?></p>
-            <?php else: ?>
-            <p class="m-0 text-center"><?= date("M d, Y" , strtotime($from)). ' - '.date("M d, Y" , strtotime($to)) ?></p>
+            <?php if ($from == $to) : ?>
+                <p class="m-0 text-center"><?= date("M d, Y", strtotime($from)) ?></p>
+            <?php else : ?>
+                <p class="m-0 text-center"><?= date("M d, Y", strtotime($from)) . ' - ' . date("M d, Y", strtotime($to)) ?></p>
             <?php endif; ?>
             <hr>
-			<table class="table table-hover table-bordered">
+            <table class="table table-hover table-bordered">
                 <thead>
                     <tr>
                         <th class="text-left">Account Name</th>
                         <th class="text-left">debit</th>
                         <th class="text-left">Credit</th>
-                        <th class="text-left">Balance</th>
+                        <!-- <th class="text-left">Balance</th> -->
                     </tr>
                 </thead>
                 <tbody>
-
-                    <?php 
-                     $balance = 0;
-                     $groupByAccountByBalance=[];
+                    <?php
+                    $balance = 0;
+                    $groupBalance = 0;
+                    $groupByAccountByBalance = [];
                     $groupByAccount = $conn->query("select g.id AS group_id,
                     g.name AS group_name,
                     al.id AS account_id,
@@ -89,59 +94,111 @@ $id= $_settings->userdata('id');
                 LEFT JOIN
                     account_list al  ON ji.account_id  = al.id
                 WHERE ji.journal_id IN (SELECT id FROM journal_entries where user_id = '{$id}' and date(journal_date) BETWEEN '{$from}' and '{$to}' )
+                and g.id  NOT IN (9,10)
                 GROUP BY
                     g.id, al.id
                 ORDER BY
                     g.id, al.id");
-                    while($arow = $groupByAccount->fetch_assoc()){
-                        $balance+= $arow['balance'];
-                        $groupByAccountByBalance[$arow['group_name']][]=$arow;
+                    while ($arow = $groupByAccount->fetch_assoc()) {
+                        $groupByAccountByBalance[$arow['group_name']][] = $arow;
+                        $balance += $arow['balance'];
                     }
                     $groupNames = array_keys($groupByAccountByBalance);
-                    $i = 0; 
-                    
-                    while (isset($groupNames[$i])):
+                    $i = 0;
+
+                    while (isset($groupNames[$i])) :
                         $groupName = $groupNames[$i];
-                        $i++; 
+                        $i++;
                     ?>
-                    <tr>
-                        <th colspan="4" class="text-center"><?= $groupName ?></th>
-                    </tr> 
+                        <tr>
+                            <th colspan="4" class="text-left"><?= $groupName ?></th>
+                        </tr>
+                        <?php
+                        $groupBalance = 0;
+                        $Accounts = $groupByAccountByBalance[$groupName];
+                        $j = 0;
+                        while (isset($Accounts[$j])) :
+                            $accountRow = $Accounts[$j];
+                            $groupBalance += $accountRow['balance'];
+                            $j++;
+                        ?>
+                            <tr>
+                                <th class="text-left" colspan="1"><?= $accountRow['account_name'] ?></th>
+                                <th class="text-left" colspan="1"><?= $accountRow['total_debit'] > 0 ? format_num($accountRow['total_debit']) : "" ?></th>
+                                <th class="text-left" colspan="1"><?= $accountRow['total_credit'] < 0 ? format_num(abs($accountRow['total_credit'])) : "" ?></th>
+                                <th class="text-left" colspan="1"></th>
+                            </tr>
+                        <?php endwhile; ?>
+                        <th colspan="3" class="text-left">Balance</th>
+                        <th class="text-left"><?= format_num($groupBalance) ?></th>
+                    <?php endwhile; ?>
+
                     <?php
-                    $groupBalance = 0; 
-                    $Accounts = $groupByAccountByBalance[$groupName];
-                    $j=0;
-                    while (isset($Accounts[$j])): 
-                        $accountRow =$Accounts[$j];
-                        $groupBalance+= $accountRow['balance'];
-                        $j++;                     
-                    ?> 
-                     <tr>
-                        <th class="text-left" colspan="1"><?= $accountRow['account_name'] ?></th>
-                        <th class="text-left" colspan="1"><?= format_num($accountRow['total_debit']) ?></th>
-                        <th class="text-left" colspan="1"><?= format_num($accountRow['total_credit']) ?></th>
-                        <th class="text-left" colspan="1"><?= format_num($accountRow['balance']) ?></th>
-                    </tr>                    
-                    <?php endwhile; ?> 
-                    <th colspan="3" class="text-left">Balance</th>
-                    <th class="text-left"><?= format_num($groupBalance) ?></th>
-                    <?php endwhile; ?>  
+                    $sundarybalance = 0;
+                    $sundarygroupByAccountByBalance = [];
+                    $groupByAccount = $conn->query("select g.id AS group_id,
+                    g.name AS group_name,
+                    al.id AS account_id,
+                    al.name AS account_name,
+                    COALESCE(SUM(CASE WHEN ji.type = 1 THEN ji.amount ELSE 0 END), 0) AS total_debit,
+                    COALESCE(SUM(CASE WHEN ji.type = 2 THEN ji.amount ELSE 0 END), 0) AS total_credit,
+                    COALESCE(SUM(CASE WHEN ji.type = 1 THEN ji.amount ELSE -ji.amount END), 0) AS balance
+                FROM
+                    journal_items ji
+                JOIN
+                    group_list g ON ji.group_id  = g.id
+                LEFT JOIN
+                    account_list al  ON ji.account_id  = al.id
+                WHERE ji.journal_id IN (SELECT id FROM journal_entries where user_id = '{$id}' and date(journal_date) BETWEEN '{$from}' and '{$to}' )
+                and g.id IN (9,10)
+                GROUP BY
+                    g.id, al.id
+                ORDER BY
+                    g.id, al.id");
+                    while ($arow = $groupByAccount->fetch_assoc()) {
+                        $sundarygroupByAccountByBalance[$arow['group_name']][] = $arow;
+                        $sundarybalance += $arow['balance'];
+                    }
+                    $sundarygroupNames = array_keys($sundarygroupByAccountByBalance);
+                    $i = 0;
+
+                    while (isset($sundarygroupNames[$i])) :
+                        $sundarygroupName = $sundarygroupNames[$i];
+                        $sundarygroupBalance = 0;
+                        $sundaryAccounts = $sundarygroupByAccountByBalance[$sundarygroupName];
+                        $j = 0;
+                        while (isset($sundaryAccounts[$j])) {
+                            $sundaryaccountRow = $sundaryAccounts[$j];
+                            $sundarygroupBalance += $sundaryaccountRow['balance'];
+                            $j++;
+                        }
+                        $i++;
+                    ?>
+                        <tr>
+                            <th class="text-left" colspan="1"><?= $sundarygroupName ?></th>
+                            <th class="text-left" colspan="1"><?= $sundarygroupBalance > 0 ? format_num($sundarygroupBalance) : "" ?></th>
+                            <th class="text-left" colspan="1"><?= $sundarygroupBalance < 0 ? format_num(abs($sundarygroupBalance)) : "" ?></th>
+                            <th class="text-left" colspan="1"></th>
+                        </tr>
+                        <th colspan="3" class="text-left">Balance</th>
+                        <th class="text-left"><?= format_num($sundarygroupBalance) ?></th>
+                    <?php endwhile; ?>
                 </tbody>
                 <tfoot>
                     <th colspan="3" class="text-center">Total</th>
-                    <th class="text-left"><?= format_num($balance) ?></th>
+                    <th class="text-left"><?= format_num($sundarybalance + $balance) ?></th>
                 </tfoot>
-			</table>
-		</div>
-	</div>
+            </table>
+        </div>
+    </div>
 </div>
 <script>
-	$(document).ready(function(){
-        $('#filter').submit(function(e){
+    $(document).ready(function() {
+        $('#filter').submit(function(e) {
             e.preventDefault()
-            location.href="./?page=reports/working_trial_balance&"+$(this).serialize();
+            location.href = "./?page=reports/working_trial_balance&" + $(this).serialize();
         })
-        $('#print').click(function(){
+        $('#print').click(function() {
             start_loader()
             var _h = $('head').clone();
             var _p = $('#outprint').clone();
@@ -150,19 +207,18 @@ $id= $_settings->userdata('id');
             _h.append('<style>html,body{ min-height: unset !important;}</style>')
             el.append(_h)
             el.append(_p)
-             var nw = window.open("","_blank","width=900,height=700,top=50,left=250")
-             nw.document.write(el.html())
-             nw.document.close()
-             setTimeout(() => {
-                 nw.print()
-                 setTimeout(() => {
-                     nw.close()
-                     end_loader()
-                 }, 200);
-             }, 500);
+            var nw = window.open("", "_blank", "width=900,height=700,top=50,left=250")
+            nw.document.write(el.html())
+            nw.document.close()
+            setTimeout(() => {
+                nw.print()
+                setTimeout(() => {
+                    nw.close()
+                    end_loader()
+                }, 200);
+            }, 500);
         })
-		
-		$('.table td,.table th').addClass('py-1 px-2 align-middle')
-	})
-	
+
+        $('.table td,.table th').addClass('py-1 px-2 align-middle')
+    })
 </script>
---
+++ database/dump-ajms_db-202405282203.sql
@@ -0,0 +1,218 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ajms_db
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.19-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account_list`
+--
+
+DROP TABLE IF EXISTS `account_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account_list` (
+  `id` int(30) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp(),
+  `user_id` int(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `account_list_users_FK` (`user_id`),
+  CONSTRAINT `account_list_users_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account_list`
+--
+
+LOCK TABLES `account_list` WRITE;
+/*!40000 ALTER TABLE `account_list` DISABLE KEYS */;
+INSERT INTO `account_list` VALUES (51,'ICICI bank','',1,0,'2024-05-26 13:54:06',NULL,NULL),(52,'JOSHNA ENTERPRISES','',1,0,'2024-05-26 13:55:10',NULL,NULL),(53,'KALYAN AQUA','',1,0,'2024-05-26 18:44:12',NULL,NULL),(54,'Mannem Bhadraiah','',1,0,'2024-05-26 19:17:30',NULL,NULL),(55,'K Navya','',1,0,'2024-05-26 19:19:38',NULL,NULL),(56,'Bank Charges','',1,0,'2024-05-26 19:21:30',NULL,NULL);
+/*!40000 ALTER TABLE `account_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `group_list`
+--
+
+DROP TABLE IF EXISTS `group_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `group_list` (
+  `id` int(30) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '0=Inactive, 1= Active',
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 = No, 1 = Yes ',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp(),
+  `user_id` int(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `group_list_users_FK` (`user_id`),
+  CONSTRAINT `group_list_users_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `group_list`
+--
+
+LOCK TABLES `group_list` WRITE;
+/*!40000 ALTER TABLE `group_list` DISABLE KEYS */;
+INSERT INTO `group_list` VALUES (8,'BANKS','',1,0,'2024-05-26 13:51:20','2024-05-26 17:40:13',NULL),(9,'Sundary Debtors','',1,0,'2024-05-26 13:54:31','2024-05-26 13:58:12',NULL),(10,'Sundary Creditors','',1,0,'2024-05-26 19:17:48',NULL,NULL),(11,'Indirect Expenses','',1,0,'2024-05-26 19:21:15',NULL,NULL);
+/*!40000 ALTER TABLE `group_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `journal_entries`
+--
+
+DROP TABLE IF EXISTS `journal_entries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `journal_entries` (
+  `id` int(30) NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) NOT NULL,
+  `journal_date` date NOT NULL,
+  `description` text NOT NULL,
+  `user_id` int(30) DEFAULT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `journal_entries_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `journal_entries`
+--
+
+LOCK TABLES `journal_entries` WRITE;
+/*!40000 ALTER TABLE `journal_entries` DISABLE KEYS */;
+INSERT INTO `journal_entries` VALUES (14,'202405-00001','2024-04-06','',6,'2024-05-26 18:36:17',NULL),(15,'202405-00002','2024-04-06','',6,'2024-05-26 18:40:25',NULL),(16,'202405-00003','2024-04-12','',6,'2024-05-26 19:10:44',NULL),(17,'202405-00004','2024-04-15','',6,'2024-05-26 19:12:11',NULL),(18,'202405-00005','2024-04-18','',6,'2024-05-26 19:13:50',NULL),(19,'202405-00006','2024-04-18','',6,'2024-05-26 19:16:26',NULL),(20,'202405-00007','2024-04-18','',6,'2024-05-26 19:19:02',NULL),(21,'202405-00008','2024-04-20','',6,'2024-05-26 19:20:25',NULL),(22,'202405-00009','2024-04-30','',6,'2024-05-26 19:22:14',NULL);
+/*!40000 ALTER TABLE `journal_entries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `journal_items`
+--
+
+DROP TABLE IF EXISTS `journal_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `journal_items` (
+  `journal_id` int(30) NOT NULL,
+  `account_id` int(30) NOT NULL,
+  `group_id` int(30) NOT NULL,
+  `amount` float NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `type` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1 = Debit, 2= Credit',
+  KEY `journal_id` (`journal_id`),
+  KEY `account_id` (`account_id`),
+  KEY `group_id` (`group_id`),
+  CONSTRAINT `journal_items_ibfk_1` FOREIGN KEY (`journal_id`) REFERENCES `journal_entries` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `journal_items_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `account_list` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `journal_items_ibfk_3` FOREIGN KEY (`group_id`) REFERENCES `group_list` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `journal_items`
+--
+
+LOCK TABLES `journal_items` WRITE;
+/*!40000 ALTER TABLE `journal_items` DISABLE KEYS */;
+INSERT INTO `journal_items` VALUES (14,51,8,30000,'2024-05-26 18:36:17',1),(14,52,9,30000,'2024-05-26 18:36:17',2),(15,51,8,30000,'2024-05-26 18:40:25',2),(15,52,9,30000,'2024-05-26 18:40:25',1),(16,53,9,699193,'2024-05-26 19:10:44',2),(16,51,8,699193,'2024-05-26 19:10:44',1),(17,51,8,699500,'2024-05-26 19:12:11',2),(17,52,9,699500,'2024-05-26 19:12:11',1),(18,52,9,85000,'2024-05-26 19:13:50',2),(18,51,8,85000,'2024-05-26 19:13:50',1),(19,52,9,85000,'2024-05-26 19:16:26',1),(19,51,8,85000,'2024-05-26 19:16:26',2),(19,52,9,515000,'2024-05-26 19:16:26',2),(19,51,8,515000,'2024-05-26 19:16:26',1),(20,54,10,500000,'2024-05-26 19:19:02',1),(20,51,8,500000,'2024-05-26 19:19:02',2),(21,55,10,15000,'2024-05-26 19:20:25',1),(21,51,8,15000,'2024-05-26 19:20:25',2),(22,56,11,45.42,'2024-05-26 19:22:14',1),(22,51,8,45.42,'2024-05-26 19:22:14',2);
+/*!40000 ALTER TABLE `journal_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `system_info`
+--
+
+DROP TABLE IF EXISTS `system_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `system_info` (
+  `id` int(30) NOT NULL AUTO_INCREMENT,
+  `meta_field` text NOT NULL,
+  `meta_value` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `system_info`
+--
+
+LOCK TABLES `system_info` WRITE;
+/*!40000 ALTER TABLE `system_info` DISABLE KEYS */;
+INSERT INTO `system_info` VALUES (1,'name','Accounting Journal Management System'),(6,'short_name','AJMS - PHP'),(11,'logo','uploads/logo-1643680475.png'),(13,'user_avatar','uploads/user_avatar.jpg'),(14,'cover','uploads/cover-1643680511.png'),(15,'content','Array'),(16,'email','info@xyzcompany.com'),(17,'contact','09854698789 / 78945632'),(18,'from_time','11:00'),(19,'to_time','21:30'),(20,'address','XYZ Street, There City, Here, 2306');
+/*!40000 ALTER TABLE `system_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(250) NOT NULL,
+  `middlename` text DEFAULT NULL,
+  `lastname` varchar(250) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 0,
+  `status` int(1) NOT NULL DEFAULT 1 COMMENT '0=not verified, 1 = verified',
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Adminstrator',NULL,'Admin','admin','0192023a7bbd73250516f069df18b500','uploads/avatar-1.png?v=1643703899',NULL,1,1,'2021-01-20 14:02:37','2022-02-01 16:24:59'),(5,'Claire',NULL,'Blake','cblake','4744ddea876b11dcb1d169fadf494418','uploads/avatar-5.png?v=1643704129',NULL,2,1,'2022-02-01 16:28:49','2022-02-01 16:28:49'),(6,'sri manikanta aqua traders',NULL,'bhargavi','bhargavi','92d43b8d661f4cd96f0190c6a3933618',NULL,NULL,1,1,'2024-05-26 13:47:49','2024-05-26 21:59:55');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'ajms_db'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-28 22:03:50
